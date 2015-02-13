ea00d87e71d672d0c2c668f765af4cc1919625f3
Hamed Nourhani <info@itstar.ir>
2015-02-13 10:37:19 +0330

    first commit
    first installation

---
+++ _/js/script.js
@@ -0,0 +1,1 @@
+function hello(){console.log("hello my friends  and me")}function text(){console.log("text   ...")}
---
+++ config.rb
@@ -0,0 +1,5 @@
+require 'susy'
+css_dir = "_/css"          #where the CSS will saved
+sass_dir = "_/components/sass"           #where our .scss files are
+javascripts = "_/js"
+output_style = :nested
