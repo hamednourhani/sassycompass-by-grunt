47712417ef1305cf4785d365f2cf592ceaf36cc3
Hamed Nourhani <info@itstar.ir>
2015-02-25 18:50:56 +0330

    breakpoints added

--- config.rb
+++ config.rb
@@ -2,4 +2,6 @@ require 'susy'
 css_dir = "_/css"          #where the CSS will saved
 sass_dir = "_/components/sass"           #where our .scss files are
 javascripts = "_/js"
+images_dir = "_/images"
 output_style = :nested
+relative_assets = true
